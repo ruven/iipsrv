78f4331c8c72885d47d6a99255c1088f9c8b282b
Ruven <gruvster@gmail.com>
2010-09-15 16:22:08 +0000

    * Modified IIPImage.cc to enable handling of images with spectral band   indices with mixtures of 3 or 4 digits. For example, H1_pyr_000_090.tif   and H1_pyr_2500_090.tif.

--- src/IIPImage.cc
+++ src/IIPImage.cc
@@ -29,6 +29,7 @@
 
 #include <cstdio>
 #include <sys/stat.h>
+#include <sstream>
 
 
 using namespace std;
@@ -128,17 +129,17 @@ void IIPImage::testImageType()
     // Check for sequence
     glob_t gdat;
     string filename = path + fileNamePattern + "000_090.*";
-      
+
     if( glob( filename.c_str(), 0, NULL, &gdat ) != 0 ){
       globfree( &gdat );
-      string message = path + fileNamePattern + string( " is neither a file or part of an image sequence" );
-      throw message;      
-    }  
+      string message = path + string( " is neither a file or part of an image sequence" );
+      throw message;
+    }
     if( gdat.gl_pathc != 1 ){
       globfree( &gdat );
       string message = string( "There are multiple file extensions matching " )  + filename;
-      throw message;      
-    }  
+      throw message;
+    }
 
     string tmp( gdat.gl_pathv[0] );
     globfree( &gdat );
@@ -205,7 +206,7 @@ void IIPImage::measureVerticalAngles()
     string tmp( gdat.gl_pathv[i] );
     int len = tmp.length() - type.length() - 1;
     string sequence_no = tmp.substr( len-3, 3 );
-    angle = atoi( sequence_no.c_str() );
+    istringstream(sequence_no) >> angle;
     verticalAnglesList.push_front( angle );
   }
 
@@ -232,21 +233,22 @@ void IIPImage::measureHorizontalAngles()
 
   if( glob( filename.c_str(), 0, NULL, &gdat ) != 0 ){
     globfree( &gdat );
-  }  
+  }
 
   for( i=0; i < gdat.gl_pathc; i++ ){
 
     // Extract angle no from path name.
     int angle;
     string tmp( gdat.gl_pathv[i] );
-    int len = tmp.length() - type.length() - 1;
-    string horizontalAnglesList_no = tmp.substr( len-7, 3 );
-    angle = atoi( (char*) horizontalAnglesList_no.c_str() );
+    int start = string(fileSystemPrefix + imagePath + fileNamePattern).length();
+    int end = tmp.find_last_of("_");
+    string n = tmp.substr( start, end-start );
+    istringstream(n) >> angle;
     horizontalAnglesList.push_front( angle );
   }
 
   horizontalAnglesList.sort();
-  
+
   globfree( &gdat );
 
 #endif
@@ -283,8 +285,9 @@ const string IIPImage::getFileName( int seq, int ang )
 
   if( isFile ) return fileSystemPrefix+imagePath;
   else{
+    // The angle or spectral band indices should be a minimum of 3 digits when padded
     snprintf( name, 1024,
-	      "%s%s%.3d_%.3d.%s", (fileSystemPrefix+imagePath).c_str(), fileNamePattern.c_str(),
+	      "%s%s%03d_%03d.%s", (fileSystemPrefix+imagePath).c_str(), fileNamePattern.c_str(),
 	      seq, ang, type.c_str() );
     
     string file( name );
