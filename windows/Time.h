7f0ef85d619d6800f8f58d24482ce5460af317e8
Ruven <ruven@free.fr>
2014-01-24 18:24:40 +0100

    Added missing std::isinfinite function for Windows Visual Studio compilation.

--- windows/Time.h
+++ windows/Time.h
@@ -36,5 +36,8 @@ char *strptime(const char*, const char*, struct tm*);
 int gettimeofday(struct timeval*, struct timezone*);
 double round(double);
 
+// isfinite missing from MSVC compiler
+static inline bool isfinite(double a) { return !_finite(a); }
+
 #endif
 #endif
